92c89d5d7cb046e9082685befeebea8f6e3e758a
Jonathan Gallimore <jon@jrg.me.uk>
2021-05-01 23:17:40 +0100

    Setup to get the signature tests running

--- src/test/script/openejb/tck/commands/JavaTestCommand.groovy
+++ src/test/script/openejb/tck/commands/JavaTestCommand.groovy
@@ -332,6 +332,8 @@ class JavaTestCommand
                     sysproperty(key: "SYSTEMROOT", value: System.getenv('SystemRoot'))
                 }
 
+                sysproperty(key: "bin.dir", value: "${javaeeCtsHome}/bin")
+
                 // Include system properties
                 arg(value: "-EsysProps")
 
